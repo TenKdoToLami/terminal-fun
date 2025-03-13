658f074362dc88de0d9e33ca4f7789be81082416
TenKdoToLami <honzajanbartos@seznam.cz>
2025-03-13 20:46:17 +0100

    Fixing file permissions to default values

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,38 @@
 
 #include "TerminalControl.h"
 #include "OneSymbol.h"
-
+#include "TerminalEffects.h"
 
 int main(void)
 {
-    OneSymbol test('#', Colors::RED, Colors::GREEN);
-    std::cout << test << test;
-    std::cout << test.foregroundColor.getBlue() << std::endl;
+    //OneSymbol test('#', Colors::RED, Colors::GREEN);
+    //std::cout << test << test;
+    //std::cout << test.foregroundColor.getBlue() << std::endl;
+    
+    //TerminalControl test;
+    //test.getTerminalSize();
+    //test.setTerminalSize();
+    //test.printTerminal();
+    //char a;
+    //std::cin >> a;
+    //test.clearTerminal();
+    //TerminalEffects::changeColorEffect(test, Colors::BLUE);
+    //test.printTerminal();
+    //std::cin >> a;
+    //test.clearTerminal();
+    
+
+
+    TerminalControl test;
+    test.getTerminalSize();
+    test.setTerminalSize();
+    test.printTerminal();
+    char a;
+    std::cin >> a;
+    test.clearTerminal();
+    TerminalEffects::invertColorEffect(test);
+    test.printTerminal();
+    std::cin >> a;
+    test.clearTerminal();
     return 0;
 }
